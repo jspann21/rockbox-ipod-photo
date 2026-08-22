33d7c11317518af55b7de0beb287d50e62886efe
jspann <jspann21@shepherds.edu>
2026-08-21 22:56:09 -0400

    ata: initialize spinup timing fallback

--- firmware/drivers/ata.c
+++ firmware/drivers/ata.c
@@ -394,7 +394,7 @@ static int ata_transfer_sectors(uint64_t start,
     long timeout;
     int count;
     void* buf;
-    long spinup_start;
+    long spinup_start = current_tick;
 #ifdef HAVE_ATA_DMA
     bool usedma = false;
 #endif
