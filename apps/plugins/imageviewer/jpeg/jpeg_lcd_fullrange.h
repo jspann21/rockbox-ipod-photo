1dcb79e7579aaa89b2afd8ed568542a9b4d290a8
jspann <jspann21@shepherds.edu>
2026-08-26 19:00:44 -0400

    JPEG: expose continuous LCD2 stream writer

--- apps/plugins/imageviewer/jpeg/jpeg_lcd_fullrange.h
+++ apps/plugins/imageviewer/jpeg/jpeg_lcd_fullrange.h
@@ -10,6 +10,12 @@
 #include "cpu.h"
 #include "hwcompat.h"
 
+bool jpeg_lcd_stream_begin(int x, int y, int width, int height);
+bool jpeg_lcd_stream_write_yuv420(unsigned char * const src[3],
+                                  int stride, int rows);
+bool jpeg_lcd_stream_end(void);
+void jpeg_lcd_stream_abort(void);
+
 bool jpeg_lcd_blit_yuv420_fullrange(unsigned char * const src[3],
                                     int src_x, int src_y, int stride,
                                     int x, int y, int width, int height);
